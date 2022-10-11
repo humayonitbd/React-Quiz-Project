750679ef3a2d7dab5d642d0fce9d91a41707861b
humayonitbd <humayonforid44@gmail.com>
2022-10-11 15:01:36 +0600

    code customize

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -6,11 +6,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const QuizDetails = ({quiz}) => {
-    // console.log(quiz)
     const {options, question, correctAnswer, id} = quiz;
-    
-    // console.log(0)
-
+  
     const iconClick = (correctAnswer) =>{
         toast(correctAnswer,{
             position:"top-center"
@@ -19,9 +16,7 @@ const QuizDetails = ({quiz}) => {
     }
 
     const optionHandler = (e) =>{
-        // console.log(e.target.innerText)
         const rightAnswer = e.target.innerText;
-        // console.log(correctAnswer)
         if(correctAnswer === rightAnswer){
             toast.success('Right Answer!!!',{
                 position:"top-center"
@@ -33,10 +28,6 @@ const QuizDetails = ({quiz}) => {
         }
 
     }
-
-
-
-
 
 
     return (
@@ -52,11 +43,6 @@ const QuizDetails = ({quiz}) => {
                         options.map((option, idx) => <p key={idx} onClick={(e)=>optionHandler(e)} className='border-2 hover:bg-slate-300 border-rose-900 py-3 rounded lg:mx-40 mb-3'>{option}</p>)
                     }
 
-
-                    {/* <p className='border-2 border-rose-900 py-3 rounded mx-40'>{options[0]}</p>
-                    <p>{options[1]}</p>
-                    <p>{options[2]}</p>
-                    <p>{options[3]}</p> */}
                 </div>
                 
                 
